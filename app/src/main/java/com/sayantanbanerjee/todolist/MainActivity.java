e897e643ae4c6ed8cc7851bf0a4866d866ceea15
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-07-05 16:37:57 +0530

    Splash Activity

--- app/src/main/java/com/sayantanbanerjee/todolist/MainActivity.java
+++ app/src/main/java/com/sayantanbanerjee/todolist/MainActivity.java
@@ -2,13 +2,50 @@ package com.sayantanbanerjee.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        image = (ImageView) findViewById(R.id.imageView);
+        image.animate().alphaBy(1f).setDuration(3000);
+        new CountDownTimer(3000,1000){
+
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }.start();
+
+
+
+
+
+
     }
 }

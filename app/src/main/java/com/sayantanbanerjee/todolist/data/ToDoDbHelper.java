9c3232cf50eecde482bbad7ffba8b199c22e05e0
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-07-06 13:29:07 +0530

    ToDoDbHelper Added. Table created

---
+++ app/src/main/java/com/sayantanbanerjee/todolist/data/ToDoDbHelper.java
@@ -0,0 +1,35 @@
+package com.sayantanbanerjee.todolist.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.sayantanbanerjee.todolist.data.ToDoContract.ToDoEntry;
+
+public class ToDoDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = ToDoDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "ToDo.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public ToDoDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + ToDoEntry.TABLE_NAME + " ("
+                + ToDoEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ToDoEntry.COLUMN_HEADING + " TEXT NOT NULL, "
+                + ToDoEntry.COLUMN_MESSAGE + " TEXT NOT NULL, "
+                + ToDoEntry.COLUMN_DATE + " TEXT NOT NULL, "
+                + ToDoEntry.COLUMN_TIME + " TEXT NOT NULL);";
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}

34cf54b16ed48a6bf3ce508bef0f021e427b0a15
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-07-06 08:47:22 +0530

    CUstomized menu for Edit activity AND colour formatted

--- app/src/main/java/com/sayantanbanerjee/todolist/EditActivity.java
+++ app/src/main/java/com/sayantanbanerjee/todolist/EditActivity.java
@@ -1,8 +1,12 @@
 package com.sayantanbanerjee.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -11,5 +15,30 @@ public class EditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
         getSupportActionBar().setTitle("Edit To-Do");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.save:
+
+                return true;
+
+            case R.id.delete:
+
+                return true;
+
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(EditActivity.this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

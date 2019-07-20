d485a0c72feea89cc1911a160d61afcd08930c50
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-07-20 08:13:57 +0530

    Creating menu in ListActivity

--- app/src/main/java/com/sayantanbanerjee/todolist/ListActivity.java
+++ app/src/main/java/com/sayantanbanerjee/todolist/ListActivity.java
@@ -1,6 +1,7 @@
 package com.sayantanbanerjee.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
@@ -10,9 +11,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sayantanbanerjee.todolist.data.ToDoContract;
@@ -59,6 +64,31 @@ public class ListActivity extends AppCompatActivity implements LoaderManager.Loa
 
         getSupportLoaderManager().initLoader(TODO_LOADER, null, this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_listactivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.deletePast:
+                
+                return true;
+
+            case R.id.todayToDo:
+
+                return true;
+
+            case R.id.allToDo:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
